22ae3b6bd2e3e40ea41d49e3680661095f168390
min <wyes91622@gmail.com>
2022-01-17 16:28:26 +0800

    add testData in Inventory page

--- src/views/Inventory.tsx
+++ src/views/Inventory.tsx
@@ -1,6 +1,24 @@
 import { Container, Form, Row, Col } from "react-bootstrap";
+import InventoryCard from "components/inventory/inventoryCard";
 
 const Inventory = () => {
+  const testData = Array.from({ length: 5 }).map((_, index) => {
+    return {
+      type: "測試類別",
+      name: "test",
+      purchase_price: index,
+      quantity: 20,
+      purchase_time: new Date(2021, 0, 13)
+    };
+  });
+  let inventoryCardList = [];
+  for (let i = 0; i < testData.length; i++) {
+    inventoryCardList.push(
+      <Col md={3} className="mt-3" key={i}>
+        <InventoryCard inventoryInfo={testData[i]} />
+      </Col>
+    );
+  }
   return (
     <Container>
       <Row>
@@ -13,6 +31,7 @@ const Inventory = () => {
           </Form.Select>
         </Col>
       </Row>
+      <Row>{inventoryCardList}</Row>
     </Container>
   );
 };
