6f3f07dec8689a0bb2814acf8dec1f4d972f1f58
kukina622 <wyes91622@gmail.com>
2022-02-03 02:23:22 +0800

    Display inventoryCard will change according to the type change

--- src/components/inventory/inventoryTypeSelect.tsx
+++ src/components/inventory/inventoryTypeSelect.tsx
@@ -1,12 +1,50 @@
 import { Form } from "react-bootstrap";
+import { useContext } from "react";
+import { inventoryContext } from "context/inventoryContext";
+import { IinventoryInfo } from "types/inventory";
 
 const InventoryTypeSelect = () => {
+  const {
+    inventoryInfoList,
+    setShowInventoryCardList,
+    generateShowInventoryCardList
+  } = useContext(inventoryContext);
+  const inventoryTypeList: string[] = inventoryInfoList.map(
+    ({ inventory_type }: IinventoryInfo) => inventory_type
+  );
+  const inventoryTypeListNoRepeat: string[] = Array.from(
+    new Set(inventoryTypeList)
+  );
+
+  function handleInventoryTypeChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedValue: number = parseInt(e.target.value);
+    let filterInventoryInfoList: IinventoryInfo[] = inventoryInfoList;
+    if (selectedValue !== -1) {
+      const selectedType: string = inventoryTypeListNoRepeat[selectedValue];
+      filterInventoryInfoList = inventoryInfoList.filter(
+        ({ inventory_type }: IinventoryInfo) => inventory_type === selectedType
+      );
+    }
+    const _showInventoryCardList = generateShowInventoryCardList(
+      filterInventoryInfoList
+    );
+    setShowInventoryCardList(_showInventoryCardList);
+  }
+
   return (
-    <Form.Select aria-label="Default select" className="mt-3">
-      <option>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+    <Form.Select
+      aria-label="Default select"
+      className="mt-3"
+      onChange={(e) => handleInventoryTypeChange(e)}
+    >
+      <option value="-1">選擇存貨類別</option>
+      {inventoryTypeListNoRepeat.map((inventoryType: string, key) => {
+        return (
+          <option value={key} key={key}>
+            {inventoryType}
+          </option>
+        );
+      })}
     </Form.Select>
   );
 };
