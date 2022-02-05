fb2ac1e3e7ab83056ecb6662a6fd229537078e2d
kukina622 <wyes91622@gmail.com>
2022-02-06 03:34:14 +0800

    add IcustomerInfo interface in types/customer

---
+++ src/types/customer.ts
@@ -0,0 +1,10 @@
+export interface IcustomerInfo {
+  customer_id: number;
+  customer_name: string;
+  tax_ID?: string;
+  contact_person?: string;
+  phone?: string;
+  fax_number?: string;
+  customer_email?: string;
+  customer_address?: string;
+}
