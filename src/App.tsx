3ea5e031e6a891ebd0ff15132bf90dd3df475e77
kukina622 <wyes91622@gmail.com>
2022-02-18 20:42:02 +0800

    add react router and add link in nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navigator from "components/Navigator";
 import Inventory from "views/Inventory";
+import Customer from "views/Customer";
 
 function App() {
   return (
     <div className="App">
       <Navigator />
-      <Inventory />
+      <Routes>
+        <Route path="/" element={<Inventory />} />
+        <Route path="customer" element={<Customer />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,12 +1,17 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Navigator = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          PSI System
+        </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link as={Link} to="customer">
+            顧客管理
+          </Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
