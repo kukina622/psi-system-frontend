f691684c2b07605fada42aa8f5bd0283fa8d16fc
kukina622 <wyes91622@gmail.com>
2022-02-19 21:36:42 +0800

    fix apiUpdateCustomerById payload type

--- src/api/customerApi.ts
+++ src/api/customerApi.ts
@@ -1,12 +1,12 @@
 import axios from "./axios";
-import { IaddCustomerInfo } from "types/customer";
+import { IaddCustomerInfo, IcustomerInfo } from "types/customer";
 
 export const apiGetAllCustomer = () => axios.get("/customer");
 
 export const apiAddCustomer = (data: IaddCustomerInfo) =>
   axios.post("/customer", data);
 
-export const apiUpdateCustomerById = (data: IaddCustomerInfo, id: number) =>
+export const apiUpdateCustomerById = (data: IcustomerInfo, id: number) =>
   axios.put(`/customer/${id}`, data);
 
 export const apiDeleteCustomerById = (id: number) =>
