cd3f02f21fa38dc8077d0f23ab594370e8a574bc
kukina622 <wyes91622@gmail.com>
2022-02-20 00:01:21 +0800

    update nav list

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -9,11 +9,18 @@ const Navigator = () => {
           PSI System
         </Navbar.Brand>
         <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
+            存貨管理
+          </Nav.Link>
           <Nav.Link as={Link} to="customer">
             顧客管理
           </Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link as={Link} to="purchase">
+            進貨
+          </Nav.Link>
+          <Nav.Link as={Link} to="sale">
+            出貨
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
