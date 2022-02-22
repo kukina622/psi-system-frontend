c7a6f3498e7f668ab7b9c3b524daf8effd470bd5
kukina622 <wyes91622@gmail.com>
2022-02-22 11:08:17 +0800

    add delete purchase and addSingle purchase

--- src/components/purchase/purchaseTable.tsx
+++ src/components/purchase/purchaseTable.tsx
@@ -1,6 +1,33 @@
+import { purchaseContext } from "context/purchaseContext";
+import { useContext } from "react";
 import { Button, Card, Table } from "react-bootstrap";
+import { IpurchaseInfo, IaddPurchaseInfo } from "types/purchase";
+import { FaCheck } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { apiAddPurchaseList } from "api/purchaseApi";
 
 const PurchaseTable = () => {
+  const { multiPurchases, setMultiPurchases } = useContext(purchaseContext);
+  function deletePurchaseByIndex(index: number) {
+    const newMultiPurchases: IpurchaseInfo[] = [...multiPurchases];
+    newMultiPurchases.splice(index, 1);
+    setMultiPurchases(newMultiPurchases);
+  }
+
+  async function addSinglePurchaseByIndex(index: number) {
+    try {
+      const _purchaseInfo: IaddPurchaseInfo = JSON.parse(
+        JSON.stringify(multiPurchases[index])
+      );
+      _purchaseInfo.purchase_time = new Date(_purchaseInfo.purchase_time);
+      await apiAddPurchaseList([_purchaseInfo]);
+      alert("送出成功");
+      deletePurchaseByIndex(index);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Card>
       <Card.Body>
@@ -21,15 +48,48 @@ const PurchaseTable = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-            </tr>
+            {multiPurchases.map(
+              (
+                {
+                  purchase_name,
+                  purchase_type,
+                  purchase_price,
+                  purchase_quantity,
+                  purchase_time,
+                  purchase_manufacturer
+                }: IpurchaseInfo,
+                index
+              ) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{purchase_name}</td>
+                    <td>{purchase_type}</td>
+                    <td>{purchase_price}</td>
+                    <td>{purchase_quantity}</td>
+                    <td>{purchase_time}</td>
+                    <td>{purchase_manufacturer}</td>
+                    <td style={{ width: "7rem" }}>
+                      <Button
+                        variant="outline-success"
+                        size="sm"
+                        className="me-2"
+                        onClick={() => addSinglePurchaseByIndex(index)}
+                      >
+                        <FaCheck />
+                      </Button>
+                      <Button
+                        variant="outline-danger"
+                        size="sm"
+                        onClick={() => deletePurchaseByIndex(index)}
+                      >
+                        <ImCross />
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              }
+            )}
           </tbody>
         </Table>
       </Card.Body>
